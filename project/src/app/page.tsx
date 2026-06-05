a6c3aca088c79aa78bf8de4ffa53a03b1f1ace95
Bao Toan <baotoanit1505@gmail.com>
2026-06-05 16:10:06 +0700

    fix benner

--- project/src/app/page.tsx
+++ project/src/app/page.tsx
@@ -1,10 +1,36 @@
-import Image from "next/image";
+import TrangChu from "./(client)/trangChu/page";
+import BoSuuTap from "./(client)/boSuuTap/page";
+import TuyChinh from "./(client)/tuyChinh/page";
+import CauChuyen from "./(client)/cauChuyen/page";
+import LienHe from "./(client)/lienHe/page";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-      </main>
-    </div>
+    <main className="flex flex-1 flex-col bg-[#F8F0E4]">
+      {/* 1. Trang chủ (Hero + Banner) */}
+      <TrangChu />
+
+      {/* 2. Bộ sưu tập */}
+      <section id="boSuuTap" className="scroll-mt-20">
+        <BoSuuTap />
+      </section>
+
+      {/* 3. Tùy chỉnh (Hỗ trợ cả hashtag #tuyChinh và #tuVan) */}
+      <section id="tuyChinh" className="scroll-mt-20">
+        <div id="tuVan">
+          <TuyChinh />
+        </div>
+      </section>
+
+      {/* 4. Câu chuyện */}
+      <section id="cauChuyen" className="scroll-mt-20">
+        <CauChuyen />
+      </section>
+
+      {/* 5. Liên hệ */}
+      <section id="lienHe" className="scroll-mt-20">
+        <LienHe />
+      </section>
+    </main>
   );
 }
